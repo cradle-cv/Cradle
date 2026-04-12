87bec3194e61c5479e2591859580d7f81081a6a4
cradle-cv <hugebear83@gmail.com>
2026-04-12 15:27:27 +0800

    update residency

--- app/residency/page.js
+++ app/residency/page.js
@@ -1,1 +1,11 @@
 
+import ResidencyClient from '@/components/ResidencyClient'
+
+export const metadata = {
+  title: '驻地 · Cradle摇篮',
+  description: '一间安静的工作室',
+}
+
+export default function ResidencyPage() {
+  return <ResidencyClient />
+}
