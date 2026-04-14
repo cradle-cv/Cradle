316f597208920b91d08ddbada48698b9516a739c
cradle-cv <hugebear83@gmail.com>
2026-04-14 22:29:06 +0800

    Add BasementPage component with metadata

--- app/residency/basement/page.js
+++ app/residency/basement/page.js
@@ -1,1 +1,11 @@
 
+import BasementSpace from '@/components/BasementSpace'
+
+export const metadata = {
+  title: '地下室 · 驻地 · Cradle摇篮',
+  description: '秘密进行中',
+}
+
+export default function BasementPage() {
+  return <BasementSpace />
+}
