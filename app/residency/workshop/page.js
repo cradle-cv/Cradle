a0e2168685d56f20a0a9992f0629b60a3a14e29f
cradle-cv <hugebear83@gmail.com>
2026-04-14 20:50:37 +0800

    Add WorkshopPage component and metadata for page

--- app/residency/workshop/page.js
+++ app/residency/workshop/page.js
@@ -1,1 +1,11 @@
 
+import WorkshopPage from './WorkshopPage'
+
+export const metadata = {
+  title: '工作台 · 驻地 · Cradle摇篮',
+  description: '杂志 · 画册编辑',
+}
+
+export default function Page() {
+  return <WorkshopPage />
+}
