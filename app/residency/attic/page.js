021bf19ea7e3f690707934abbb2252665f40ce85
cradle-cv <hugebear83@gmail.com>
2026-04-15 14:25:14 +0800

    Add AtticPage component with metadata

--- app/residency/attic/page.js
+++ app/residency/attic/page.js
@@ -1,1 +1,11 @@
 
+import AtticSpace from '@/components/AtticSpace'
+
+export const metadata = {
+  title: '阁楼 · 驻地 · Cradle摇篮',
+  description: '星空下的安静角落',
+}
+
+export default function AtticPage() {
+  return <AtticSpace />
+}
