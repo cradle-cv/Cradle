2698cc7193825779afe280f00ed6c6be3a0a14c7
cradle-cv <hugebear83@gmail.com>
2026-06-12 22:14:27 +0800

    Change artwork status from 'draft' to 'published'

--- app/studio/artworks/new/page.js
+++ app/studio/artworks/new/page.js
@@ -32,7 +32,7 @@ export default function StudioNewArtworkPage() {
     year: new Date().getFullYear(),
     description: '',
     image_url: '',
-    status: 'draft'
+    status: 'published'
   })
 
   useEffect(() => { init() }, [])
